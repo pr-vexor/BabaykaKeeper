491317a067fa203de8745e3fd4e48aaaec0dba70
markov.me <markov.me@mail.ru>
2025-11-12 19:55:07 +0300

    Переменные текста команд более читабельные

--- src/main/java/pr/vexor/telegrambot/babaykakeeper/utils/TextFields.java
+++ src/main/java/pr/vexor/telegrambot/babaykakeeper/utils/TextFields.java
@@ -2,27 +2,35 @@ package pr.vexor.telegrambot.babaykakeeper.utils;
 
 public class TextFields {
     
-    public static final String START_COMMAND_TEXT = "Привет! Я бот Бабайка-Хранитель \n\n" +
-                                    "Доступные команды:\n" +
-                                    "/getchatid  - узнать ID этого чата\n" +
-                                    "/activate   - активировать бота\n" +
-                                    "/standby   - перевести бота в режим ожидания\n" +
-                                    "/status      - посмотреть статус бота";
+    public static final String START_COMMAND_TEXT = """
+                                                    Привет! Я бот Бабайка-Хранитель 
+                                                    
+                                                    Доступные команды:
+                                                    /getchatid  - узнать ID этого чата
+                                                    /activate    - активировать бота
+                                                    /standby    - перевести бота в режим ожидания
+                                                    /status        - посмотреть статус бота
+                                                    """;
     
-    public static final String STATUS_COMMAND_TEXT = "Статус бота:\n" +
-                                    " - Состояние: %s\n" +
-                                    " - Экземпляр: %s\n" +
-                                    " - Функционал: в разработке\n\n";  
-
-        
-    public static final String CHAT_INFO_COMMAND_TEXT = "Информация о чате:\n" +
-                                    " - ID: %s\n" +
-                                    " - Тип: %s\n" +
-                                    " - Название: %s";  
-        
-    public static final String UNKNOWN_COMMAND_TEXT = "Неизвестная команда\n" +
-                                    "Используй /start для списка команд";
-        
+    public static final String STATUS_COMMAND_TEXT = """
+                                                     Статус бота:
+                                                      - Состояние: %s
+                                                      - Экземпляр: %s
+                                                      - Функционал: в разработке
+                                                     """;
+    
+    public static final String CHAT_INFO_COMMAND_TEXT = """
+                                                        Информация о чате:
+                                                         - ID: %s
+                                                         - Тип: %s
+                                                         - Название: %s
+                                                        """;
+    
+    public static final String UNKNOWN_COMMAND_TEXT = """
+                                                      Неизвестная команда
+                                                      Используй /start для списка команд
+                                                      """;
+    
     public static final String STANDBYING_SUCCESS = "Бот переведён в режим ожидания";
     public static final String STANDBYING_ERORR = "Не удалось перевести бота в режим ожидания";
     public static final String ACTIVATION_SUCCESS = "Бот активирован и будет теперь обрабатывать посты из канала";
@@ -36,6 +44,6 @@ public class TextFields {
     public static final String STANDBY_STATUS = "В ОЖИДАНИИ";
     public static final String MESSAGES_TYPE = "Личные сообщения";
     
-    public static final String BUTTON_LINK_HTML_FORMAT = "\n\n #всем_чай <a href=\"%s\">%s</a>";
+    public static final String BUTTON_LINK_HTML_FORMAT = "\n\n#всем_чай <a href=\"%s\">%s</a>";
     public static final String MESSAGE_LINK_FORMAT = "https://t.me/c/%s/%s";
 }
